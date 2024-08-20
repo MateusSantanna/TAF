04765b399daaf5fac34f4ed1820da4e1770ee481
MateusSantanna <mateussantanna2001@outlook.com>
2024-08-20 12:56:08 -0300

    feat: logic created

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-// Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
-gender = process.argv[2]; // Genero 
-height = process.argv[3]; // Altura
-barReps = process.argv[4]; // Repetições com barra
-barSeconds = process.argv[5]; // Tempo das repetições com barra 
-abs = process.argv[6]; // Abdominais
-runDistance = process.argv[7]; // Distância da corrida
-runTime = process.argv[8]; // Tempo da corrida
-swimDistance = process.argv[9]; // Distância da natação
-swimTime = process.argv[10]; // Tempo da natação
-diveTime = process.argv[11]; // Tempo de mergulho
+let gender = process.argv[2]; // Genero
+let height = process.argv[3]; // Altura
+let barReps = process.argv[4]; // Repetições com barra
+let barSeconds = process.argv[5]; // Tempo das repetições com barra
+let abs = process.argv[6]; // Abdominais
+let runDistance = process.argv[7]; // Distância da corrida
+let runTime = process.argv[8]; // Tempo da corrida
+let swimDistance = process.argv[9]; // Distância da natação
+let swimTime = process.argv[10]; // Tempo da natação
+let diveTime = process.argv[11]; // Tempo de mergulho
 
 passed = false;
 
-/*
-    Importante:
-    Se o genero (gender) for masculino, utilize a palavra "male"
-    Se o genero (gender) for feminino, utilize a palavra "female"
+if (gender === "male") {
+  if (height >= 1.7 && abs >= 41) {
+    if (barReps >= 6 || barSeconds >= 15) {
+      if (
+        (runDistance >= 3000 && runTime <= 720) ||
+        (runDistance >= 5000 && runTime <= 1200)
+      ) {
+        if (
+          (swimDistance >= 100 && swimTime <= 60) ||
+          (swimDistance >= 100 && diveTime <= 30)
+        ) {
+          passed = true;
+        }
+      }
+    }
+  }
+} else if (gender === "female") {
+  if (height >= 1.6 && abs >= 41) {
+    if (barReps >= 5 || barSeconds >= 12) {
+      if (
+        (runDistance >= 4000 && runTime <= 900) ||
+        (runDistance >= 6000 && runTime <= 1320)
+      ) {
+        if ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30) {
+          passed = true;
+        }
+      }
+    }
+  }
+} else {
+  passed = false;
+}
 
-    Sugestão para começar:
-    if (gender === "male")
-    ...
-*/
-// **sua lógica a partir daqui**
-
-
-
-/*
-    Seu código deve conter apenas UM console.log, e ele deve ser o abaixo.
-    Não altere nada pra baixo dessa linha, senão os testes não irão funcionar.
-*/
 console.log(passed);
